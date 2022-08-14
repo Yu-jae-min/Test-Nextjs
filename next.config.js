cd1530439572beaca956230f94821869433f4c18
유재민 <yoojamer@gmail.com>
2022-08-14 19:02:42 +0900

    chore : 초기 세팅 완료

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
